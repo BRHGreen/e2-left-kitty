db09c37903811bf8b82490aa74b97f5a97c45398
BRHGreen <benedictgreen@gmail.com>
2018-11-29 13:30:20 +0000

    using compose

--- src/Person.js
+++ src/Person.js
@@ -1,33 +1,28 @@
 import React from "react";
-import { Query } from "react-apollo";
+import { compose, graphql } from "react-apollo";
 import gql from "graphql-tag";
 
-const obj = {
-  one: "one",
-  two: "two"
+const Person = ({ getHousemates: { allHousemates, loading, error } }) => {
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return (
+    <div>
+      {allHousemates.map((housemate, i) => (
+        <p key={i}>{`Username: ${housemate.username}`}</p>
+      ))}
+    </div>
+  );
 };
 
-export default () => (
-  <Query
-    query={gql`
+export default compose(
+  graphql(
+    gql`
       {
         allHousemates {
           username
         }
       }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
-      return (
-        <div>
-          {console.log("data", data)}
-          {data.allHousemates.map((housemate, i) => (
-            <p key={i}>{`Username: ${housemate.username}`}</p>
-          ))}
-        </div>
-      );
-    }}
-  </Query>
-);
+    `,
+    { name: "getHousemates" }
+  )
+)(Person);
