0b4136fd7988eb3976086363cb451e23f265f815
BRHGreen <benedictgreen@gmail.com>
2018-12-12 17:59:42 +0000

    fixed kitty parser

--- src/components/Kitty/KittyStatements.js
+++ src/components/Kitty/KittyStatements.js
@@ -7,8 +7,7 @@ import {
 } from "./graphql/kittyStatements";
 import {
   allHousemates,
-  assignHousemateToStatement,
-  housemateById
+  assignHousemateToStatement
 } from "./graphql/housemates";
 import KittyOwnerDropdown from "./KittyOwnerDropdown";
 import Dropdown from "../Common/Dropdown";
